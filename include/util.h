02f5dfa173364eeb4fe2cac0a7002b6d70e89367
mariievrcl <49845362+mariievrcl@users.noreply.github.com>
2025-12-03 08:44:17 +0100

    commentaires util.h

--- include/util.h
+++ include/util.h
@@ -10,9 +10,15 @@
 #ifndef UTIL_H
 #define UTIL_H
 
-
+// Lit une ligne depuis l'entrée standard (stdin) et supprime les retours à la ligne (\n et \r)
 void lireLigne(char *buf, int n);
+
+// Remplit le buffer avec la date actuelle au format "JJ/MM/AAAA"
 void dateAuj(char *buf);
+
+// Calcule le nombre de jours écoulés depuis une date donnée au format "JJ/MM/AAAA"
+// Renvoie un entier représentant le nombre de jours
 int joursDepuis(const char *date); /* JJ/MM/AAAA -> jours écoulés depuis cette date */
 
-#endif
+#endif // UTIL_H
+// Fin de la protection contre inclusion multiple
